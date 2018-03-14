757dbef3331932faedf8bd57447657547f5042d2
Eric Neo <eric.neo@c02n9fftg3qc.dg.domain.com.au>
2018-03-14 16:13:56 +1100

    Adding api request

--- scripts/background.js
+++ scripts/background.js
@@ -7,48 +7,67 @@
   permission in the manifest file (or calling
   "Notification.requestPermission" beforehand).
 */
-/*
+
 function show() {
-  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
+  if (!localStorage.domainSavedSearches) {
+    return;
+  }
+
+  const savedSearches = JSON.parse(localStorage.domainSavedSearches);
+  const firstSearch = JSON.parse(savedSearches[0].searchQuery.queryBody);
+
+  const payload = {
+    listingType: 'Sale',
+    page: 1,
+    sort: { sortKey: 'DateUpdated', direction: 'Descending' },
+    locations: firstSearch.locations.map(location => ({ 'suburb': location }))
+  };
+
+  fetch('https://stage-search-api.domain.com.au/v2/listings/search', {
+    method: 'post',
+    headers: {
+      Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlNvQlBlWjdOeFU3bEVPeDZSVzhfbHVhMWtKZyIsImtpZCI6IlNvQlBlWjdOeFU3bEVPeDZSVzhfbHVhMWtKZyJ9.eyJpc3MiOiJodHRwczovL3N0YWdlLWF1dGguZG9tYWluLmNvbS5hdS92MSIsImF1ZCI6Imh0dHBzOi8vc3RhZ2UtYXV0aC5kb21haW4uY29tLmF1L3YxL3Jlc291cmNlcyIsImV4cCI6MTUyMTA4ODc3MiwibmJmIjoxNTIxMDAyMzcyLCJjbGllbnRfaWQiOiJmZS1zZXJ2ZXItc2VhcmNoLXJlc3VsdHMiLCJzY29wZSI6WyJyZWFkLWFkcyIsInJlYWQtYWdlbmN5IiwicmVhZC1saXN0aW5nIiwic2VhcmNoLWxpc3RpbmdzIl0sImp0aSI6ImY4MmQxMTZiOTg1NWU4ODY1ZDRmOGZkMjliMzNkZTI3In0.ftp5vdPMDJRl4nBzQcuJ_faU6bnTRk0XRtyFr6b2tBn5OeyuJ0S3B7Dq9HuCezcPBJ_OjGIoGdx4ahT48mORsKv7CyeG6EJH2y_bGU_zXRscWJNeqZXOHjhQ8Wp8h6LX_v8ik5-xGXp53-J-VzIbXGqoBlEWVhDrFRXPQIHnbRMmPClYem6BLfsbipLAENhqHV2MPXfDWZAITH0yUER0KIH4XOtW0Mvn4MWNAOLCui1LNuR9G7OD0diAWVeAsFZE8xnkDFJQY_j9V7fho1z6E5IX2YsK7wj5DSWv5ewtp-Il8m1egvihs0slrWMXRFfN7yOhQk8i18H_CKCEHJLLsQ',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  })
+  .then(response => (response.json()))
+  .then(myJson => { console.log(myJson); })
+  .catch(error => { console.log('Request failed', error); });
+
+  /* var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
   var hour = time[1] % 12 || 12;               // The prettyprinted hour.
   var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
   new Notification(hour + time[2] + ' ' + period, {
     icon: '48.png',
-    body: 'Time to make the toast22.'
-  });
+    body: 'Time to make the toast.'
+  }); */
 }
 
-// Conditionally initialize the options.
-if (!localStorage.isInitialized) {
-  localStorage.isActivated = true;   // The display activation.
-  localStorage.frequency = 1;        // The display frequency, in minutes.
-  localStorage.isInitialized = true; // The option initialization.
-}
+// localStorage.domainSavedSearches = '{"savedSearches":[{"searchName":"Penrith NSW 2750, Chatswood NSW 2067","url":"https://stage.domain.com.au/sale/?suburb=chatswood-nsw-2067,penrith-nsw-2750&ssubs=1&excludeunderoffer=1","searchQuery":{"queryBody":"{\"humanizedPrice\":\"Any price\",\"locations\":[\"Penrith\",\"Chatswood\"],\"listingType\":\"\"}"}},{"searchName":"rr","url":"https://stage.domain.com.au/new-homes/?ptype=new-apartments,new-house-land,new-land,town-house&bedrooms=1-5&bathrooms=1-4&price=0-250000&carspaces=0-4","searchQuery":{"queryBody":"{\"humanizedPrice\":\"0-250000\",\"locations\":[\"\"],\"listingType\":\"\"}"}},{"searchName":"w","url":"https://stage.domain.com.au/new-homes/?ptype=new-apartments,new-house-land,new-land,town-house&bedrooms=1-5&bathrooms=1-4&price=0-200000&carspaces=0-4","searchQuery":{"queryBody":"{\"humanizedPrice\":\"0-200000\",\"locations\":[\"\"],\"listingType\":\"\"}"}}]}';
 
 // Test for notification support.
 if (window.Notification) {
   // While activated, show notifications at the display frequency.
-  if (JSON.parse(localStorage.isActivated)) { show(); }
+  show();
 
-  var interval = 0; // The display interval, in minutes.
+  let interval = 0; // The display interval, in minutes.
+  const frequency = 1;
 
   setInterval(function() {
     interval++;
 
-    if (
-      JSON.parse(localStorage.isActivated) &&
-        localStorage.frequency <= interval
-    ) {
-      show();
+    if (frequency <= interval) {
+      // show();
       interval = 0;
     }
-  }, 60000);
+  }, 6000);
 }
-*/
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  // alert("message received");
-  console.log(sender.tab ?
-    "from a content script:" + sender.tab.url :
-    "from the extension");
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if(request.type === 'STORE_SAVED_SEARCH') {
+    console.log('storing saved searches: ', request.data);
+    localStorage.domainSavedSearches = JSON.stringify(request.data.savedSearchList);
+  }
+  sendResponse({ success: request.type === 'STORE_SAVED_SEARCH' });
 });
--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,13 +1,13 @@
 window.addEventListener('message', function(event) {
     if (event.data.type === 'SAVED_SEARCH_RESULTS') {
-      console.log('Domain extension is receiving saved searches...');
+      chrome.runtime.sendMessage({ type: 'STORE_SAVED_SEARCH', data: event.data.data });
     }
   }, true);
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if(request.type === 'UPDATED_SAVED_SEARCH') {
     window.postMessage({ type: 'UPDATED_SAVED_SEARCH' }, '*');
-    sendResponse({ success: true });
   }
+  sendResponse({ success: request.type === 'UPDATED_SAVED_SEARCH' });
 });
 
